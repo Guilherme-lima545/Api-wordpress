34110eee476b1cccf5526c91eece055b17c1ed8f
Guilherme <guisupergames123@gmail.com>
2024-09-11 01:42:14 -0300

    adicionando funções e campos novos

--- endpoints/user_post.php
+++ endpoints/user_post.php
@@ -3,17 +3,17 @@
 function api_user_post($request) {
   $email = sanitize_email($request['email']);
   $username = sanitize_text_field($request['username']);
-  $password = $request['password'];
+  $password = sanitize_text_field($request['password']);
 
   if(empty($email) || empty($username) || empty($password)) {
     $response = new WP_Error('error', 'Dados incompletos', ['status' => 406]);
     return rest_ensure_response($response);
-  };
+  }
 
   if(username_exists($username) || email_exists($email)) {
     $response = new WP_Error('error', 'Email ja cadastrado', ['status' => 403]);
     return rest_ensure_response($response);
-  };
+  }
 
 
   $response = wp_insert_user([
@@ -24,14 +24,14 @@ function api_user_post($request) {
   ]);
 
   return rest_ensure_response($response);
-};
+}
 
 function register_api_user_post() {
-  register_rest_route('api', '/user', [
-    'methods' => WP_REST_Server::CREATABLE,
+  register_rest_route('api', '/user',[
+    'methods' => 'POST',
     'callback' => 'api_user_post',
   ]);
-};
+}
 
 add_action('rest_api_init', 'register_api_user_post');
 
